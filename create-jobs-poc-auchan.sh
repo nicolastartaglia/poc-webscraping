9991a2a178ec0c489ca917f492da1ca6c97be519
nicolastartaglia <ntartaglia@gmail.com>
2023-04-24 23:23:16 +0200

    +Auchan

---
+++ create-jobs-poc-auchan.sh
@@ -0,0 +1,9 @@
+input="env-poc-auchan"
+while IFS= read -r line
+do
+  COUNT=`echo "$line" | awk -F"," '{print $1}'`;
+  URL=`echo "$line" | awk -F"," '{print $2}'`;
+  DISTRIBUTEUR=`echo "$line" | awk -F"," '{print $3}'`;
+  SEARCH=`echo "$line" | awk -F"," '{print $4}'`;
+  cat templates-job/template-job-poc-auchan.yaml | sed -e "s/\$COUNT/$COUNT/g" | sed -e "s/\$URL/$URL/g" | sed -e "s/\$DISTRIBUTEUR/$DISTRIBUTEUR/g" | sed -e "s/\$SEARCH/$SEARCH/g" > ./jobs-poc-auchan/job-$COUNT.yaml;
+done < "$input"
--- create-jobs-poc-carrefour.sh
+++ create-jobs-poc-carrefour.sh
@@ -5,5 +5,5 @@ do
   URL=`echo "$line" | awk -F"," '{print $2}'`;
   DISTRIBUTEUR=`echo "$line" | awk -F"," '{print $3}'`;
   SEARCH=`echo "$line" | awk -F"," '{print $4}'`;
-  cat template-job-poc-carrefour.yaml | sed -e "s/\$COUNT/$COUNT/g" | sed -e "s/\$URL/$URL/g" | sed -e "s/\$DISTRIBUTEUR/$DISTRIBUTEUR/g" | sed -e "s/\$SEARCH/$SEARCH/g" > ./jobs-poc-carrefour/job-$COUNT.yaml;
+  cat templates-job/template-job-poc-carrefour.yaml | sed -e "s/\$COUNT/$COUNT/g" | sed -e "s/\$URL/$URL/g" | sed -e "s/\$DISTRIBUTEUR/$DISTRIBUTEUR/g" | sed -e "s/\$SEARCH/$SEARCH/g" > ./jobs-poc-carrefour/job-$COUNT.yaml;
 done < "$input"
